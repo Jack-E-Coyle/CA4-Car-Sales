1859892101a00a0c2cee279f43b4c322a439c6e5
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-04-02 12:02:31 +0100

    NPM installs + enviroment.ts code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB_4EawxXtf4V-725lFJBs5_TJ8THyZNLE",
+    authDomain: "ca4-car-sales.firebaseapp.com",
+    projectId: "ca4-car-sales",
+    storageBucket: "ca4-car-sales.appspot.com",
+    messagingSenderId: "720973160042",
+    appId: "1:720973160042:web:0e0321216a171340bf7413",
+    measurementId: "G-1QQ4N78HHT"
+  }
 };
 
 /*
