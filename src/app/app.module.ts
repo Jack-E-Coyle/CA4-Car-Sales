c9e02be71946428d5962b5c17d6e0bf600841997
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-04-02 12:16:07 +0100

    Connecting application to firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AnularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
