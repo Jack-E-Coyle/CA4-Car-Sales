f449395290f6f171dba07aeb032611e43e7be2ee
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-04-02 12:09:49 +0100

    Adding to enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB_4EawxXtf4V-725lFJBs5_TJ8THyZNLE",
+    authDomain: "ca4-car-sales.firebaseapp.com",
+    projectId: "ca4-car-sales",
+    storageBucket: "ca4-car-sales.appspot.com",
+    messagingSenderId: "720973160042",
+    appId: "1:720973160042:web:0e0321216a171340bf7413",
+    measurementId: "G-1QQ4N78HHT"
+  }
 };
